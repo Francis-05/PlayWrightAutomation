3ee739fb73090a8012a34fc1a45b1dda8ff875b3
Francisdel Chris Patlingrao <francisdelchrispatlingrao>
2026-07-27 15:02:08 +0800

    new additions

---
+++ data/loginData.spec.js
@@ -0,0 +1,21 @@
+module.exports = {
+    validUser: {
+        email: "franz.rose225@gmail.com",
+        password: "admin###123"
+    },
+
+    unregisteredUser: {
+        email: "incorrectuser@g.com",
+        password: "admin###123"
+    },
+
+    invalidPasswordUser: {
+        email: "franz.rose225@gmail.com",
+        password: "incorrectpass"
+    },
+
+    invalidEmailFormat: {
+        email: "franz.rose225@@@gmail.com",
+        password: "admin###123"
+    },
+};
---
+++ pages/loginPage.js
@@ -0,0 +1,36 @@
+const {test, expect} = require('@playwright/test');
+
+class loginPage {
+    constructor(page) {
+        this.page = page;
+
+        //login feature locators
+        
+        this.emailInput = page.getByRole('textbox', { name: 'email@example.com' });
+        this.passwordInput = page.getByRole('textbox', { name: 'enter your passsword' });
+        this.loginButton = page.getByRole('button', {name: 'Login'});
+        this.toastMessage = page.locator('#toast-container');
+        this.invalidFeedbacks = page.locator('.invalid-feedback');
+        this.forgotPassword = page.getByRole('link', {name: 'Forgot password'});
+        this.registerLink = page.getByText('Don\'t have an account?');
+    }
+
+    async login (email, password) {
+        await this.emailInput.fill(email);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+
+    async clickLogin () {
+        await this.loginButton.click();
+    }
+
+    async clickForgotPassword () {
+        await this.forgotPassword.click();
+    }
+
+    async clickRegister () {
+        await this.registerLink();
+    }
+}
+module.exports = { loginPage };
