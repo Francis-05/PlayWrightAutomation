81b2ce54416a6e349733f3e84e3d54192b2ae86a
Francisdel Chris Patlingrao <francisdelchrispatlingrao>
2026-07-27 11:24:36 +0800

    remove comment

--- tests/AutomationTestingBasics.spec.js
+++ tests/AutomationTestingBasics.spec.js
@@ -10,8 +10,6 @@ test('TC-POS-01: Verify successfull login with valid credentials', async ({page}
     await page.getByRole('textbox', { name: 'email@example.com' }).fill('franz.rose225@gmail.com');
     await page.getByRole('textbox', { name: 'enter your passsword' }).fill('admin###123');
     await page.getByRole('button', { name: 'Login' }).click();
-    // await expect (page.getByText('*Email is required')).toBeVisible();
-    // await expect (page.getByText('*Password is required')).toBeVisible();
 
     await page.pause();
     console.log("Test Run Success");
