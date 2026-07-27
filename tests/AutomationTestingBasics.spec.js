5bf37ce86435963606b4fe17369d2a218ddf37e7
Francisdel Chris Patlingrao <francisdelchrispatlingrao>
2026-07-27 11:16:18 +0800

    Initial commit automation test

---
+++ tests/AutomationTestingBasics.spec.js
@@ -0,0 +1,34 @@
+const {test, expect} = require('@playwright/test');
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
+});
+
+//login feature (happy path cases)
+test('TC-POS-01: Verify successfull login with valid credentials', async ({page}) => {
+
+    await page.getByRole('textbox', { name: 'email@example.com' }).fill('franz.rose225@gmail.com');
+    await page.getByRole('textbox', { name: 'enter your passsword' }).fill('admin###123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    // await expect (page.getByText('*Email is required')).toBeVisible();
+    // await expect (page.getByText('*Password is required')).toBeVisible();
+
+    await page.pause();
+    console.log("Test Run Success");
+});
+
+//login feature (negative cases)
+test.only('TC-POS-02: Verify error message for login with unregistered email', async ({page}) => {
+
+    await page.getByRole('textbox', { name: 'email@example.com' }).fill('incorrectuser@g.com');
+    await page.getByRole('textbox', { name: 'enter your passsword' }).fill('admin###123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    // await expect(page.getByRole('alert', { name: 'Incorrect email or passssword.' })).toBeVisible();
+
+    const errorMessage = page.locator('#toast-container');
+    await expect(errorMessage).toBeVisible();
+    await expect(errorMessage).toHaveText('Incorrect email or password.');
+
+    console.log("Test Run Success");
+    await page.pause();
+});
