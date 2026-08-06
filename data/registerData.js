28a4246541c057cedad29d75cb062186a9ce0d6a
Francisdel Chris Patlingrao <francisdelchrispatlingrao>
2026-08-06 11:57:03 +0800

    add method for random email & change dropdown data to label

--- data/registerData.js
+++ data/registerData.js
@@ -1,21 +1,24 @@
-const randomEmail = `test${Date.now()}${Math.floor(Math.random() * 1000)}@g.com`;
+function generateUniqueEmail () {
+    return `test${Date.now()}${Math.floor(Math.random() * 1000)}@g.com`;
+}
 
 module.exports = {
+    
+    generateUniqueEmail,
 
     regfieldCredentials : {
         fname: "John",
         lname: "Doe",
-        email: randomEmail,
         phoneNum: "1234567890",
         password: "Admin#123",
         confirmPass: "Admin#123",
     },
 
     regdropdownOccupation : {
-        occupation1: "Doctor",
-        occupation2: "Student",
-        occupation3: "Engineer", 
-        occupation4: "Scientist",
+        doctor: "Doctor",
+        student: "Student",
+        engineer: "Engineer", 
+        scientist: "Scientist",
     },
 
     passwordMismatch: {
