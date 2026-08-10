31393217a5d9f640963e39bd12f2e12b0257286b
Francisdel Chris Patlingrao <francisdelchrispatlingrao>
2026-08-10 16:48:20 +0800

    locator fix on nav test script (3)

--- pages/registerPage.js
+++ pages/registerPage.js
@@ -25,7 +25,7 @@ class registerPage extends basePage {
         this.passFieldError = page.locator('div.form-group', {has: this.password}).locator('div.invalid-feedback div');
         this.confirmpassFieldError = page.locator('div.form-group', {has: this.confirmPass}).locator('div.invalid-feedback div');
         this.checkBoxError = page.getByText('*Please check above checkbox');
-        this.loginHereBtn = page.locator('.login-wrapper-footer-text');
+        this.loginHereBtn = page.getByText('Login here');
 
     }
     
